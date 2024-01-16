a7634527874807780022bca0bec83925cc401e00
Migs612 <gutierrezsivilamanuel@gmail.com>
2024-01-16 20:26:38 +0100

    Agregado el primer ejercicio

---
+++ HTMLdePrueba/script.js
@@ -0,0 +1,11 @@
+function openModal() {
+    document.getElementById('videoModal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('videoModal').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    closeModal();
+});
